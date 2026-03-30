967083a269319fb9898adba5b1b3137841e4d6b2
jgunstone <gunstone.john@gmail.com>
2026-03-30 11:07:26 +0100

    add check openstudio test. skip if not installed.

--- tests/test_16EnergyModel.py
+++ tests/test_16EnergyModel.py
@@ -1,18 +1,22 @@
+import pytest
+
 try:
-    import openstudio
     import honeybee_energy
+    import openstudio
 except ImportError as e:
     print("honeybee-energy is not installed. Please install it to run this test.")
-    raise e
-    exit(1)
 
+
+@pytest.mark.skipif(
+    not pytest.importorskip("openstudio", reason="openstudio is not installed"),
+    reason="openstudio is not installed",
+)
 def test_set_energy_model_log_level():
     # Not implemented yet
-    import openstudio
+
     try:
         openstudio.Logger.instance().standardOutLogger().setLogLevel(openstudio.Fatal)
         assert True
     except Exception as e:
         print("Failed to set OpenStudio log level.")
         assert False
-        
